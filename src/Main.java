1502d51114a187a12386443a8d2cf4b5fc92cee6
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-10-27 23:10:57 +0200

    rework of printing final line. Add dynamic-changeble size of line and replacing null symbols by space

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        GlyphFactory factory = new GlyphFactory();
         TextLine line = new TextLine();
+        try{
+            line.add('H', 0);
+            line.add('e', 1);
+            line.add('l', 2);
+            line.add('l', 3);
+            line.add('o', 4);
+            line.add(' ', 5);
+            line.add('W', 6);
+            line.add('o', 7);
+            line.add('r', 8);
+            line.add('l', 9);
+            line.add('d', 10);
 
-        line.add('H', 0);
-        line.add('e', 1);
-        line.add('l', 2);
-        line.add('l', 3);
-        line.add('o', 4);
+            line.print();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
 
-        line.add(' ', 5);
-
-        line.add('W', 6);
-        line.add('o', 7);
-        line.add('r', 8);
-        line.add('l', 9);
-        line.add('d', 10);
-
-        line.print();
     }
 }
--- src/TextLine.java
+++ src/TextLine.java
@@ -5,21 +5,30 @@ import java.util.List;
 public class TextLine {
 
     private List<ConcreteGlyph> line = new ArrayList<>();
+    private int max = -1;
 
     public void add(char symbol, int position){
+        if (position < 0){
+            throw new IllegalArgumentException("Position can't be negative!");
+        }
+
         Glyph glyph = GlyphFactory.getGlyph(symbol);
         ConcreteGlyph concreteGlyph = new ConcreteGlyph(glyph, position);
         line.add(concreteGlyph);
+
+        if (position > max){
+            max = position;
+        }
     }
 
-    //TODO size of char array must depends of maximal value of "position"
-    public void print(){
-        char[] renderedSymbols = new char[line.size()];
-        line.forEach(concreteGlyph -> {
+    public void print(){;
+        char[] renderedSymbols = new char[max + 1];
+        Arrays.fill(renderedSymbols, ' ');
+
+        for (ConcreteGlyph concreteGlyph : line) {
             concreteGlyph.render();
             renderedSymbols[concreteGlyph.getPosition()] = concreteGlyph.getSymbol();
-        });
-
+        }
         System.out.println(String.valueOf(renderedSymbols));
     }
 }
